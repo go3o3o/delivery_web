c1331967c531b8879aa0e405aedaf76c70dc230e
hoyoni <hoyeon.kim2773@gmail.com>
2025-01-29 14:55:56 +0900

    [Fix] update dto change partial type

--- server/src/modules/shop/dto/update-shop-category.dto.ts
+++ server/src/modules/shop/dto/update-shop-category.dto.ts
@@ -1,5 +1,6 @@
+import { PartialType } from '@nestjs/swagger';
 import { ShopCategoryDto } from './shop-category.dto';
 
-export class UpdateShopCategoryDto extends ShopCategoryDto {
+export class UpdateShopCategoryDto extends PartialType(ShopCategoryDto) {
   readonly id?: number;
 }
