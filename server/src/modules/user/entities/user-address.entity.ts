87cf7310356d0cd53aee296636fac5915e259dd8
hoyoni <hoyeon.kim2773@gmail.com>
2025-03-03 16:41:25 +0900

    [Add] user entity
    - User
    - UserAddress

---
+++ server/src/modules/user/entities/user-address.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  RelationId,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserEntity } from './user.entity';
+
+@Entity({ name: 'UserAddress' })
+export class UserAddressEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @RelationId((self: UserAddressEntity) => self.user)
+  @Column()
+  @Index('FK_USER_ADDRESS')
+  userId: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.addresses)
+  user: UserEntity;
+
+  @Column({ type: 'varchar', length: 255, comment: '주소' })
+  address: string;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn({ nullable: true })
+  updatedDate: Date;
+
+  @DeleteDateColumn({ nullable: true })
+  deletedDate: Date;
+}
---
+++ server/src/modules/user/user.enum.ts
@@ -0,0 +1,6 @@
+export enum UserGrade {
+  Iron = 'IRON',
+  Bronze = 'BRONZE',
+  Silver = 'SILVER',
+  Gold = 'GOLD',
+}
